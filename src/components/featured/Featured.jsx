23de72171cafbbb0ee574fa1e8d82c95f8a5307d
mcj130101 <mcj130101@gmail.com>
2023-04-05 16:24:01 +0530

    added images

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -8,11 +8,11 @@ const Featured = () => {
         <div className="left">
           <h1>Find the perfect <i>freelance</i> services for your business</h1>
           <div className="search">
-          <div className="searchInput">
-            <img src="" alt="" />
-            <input type="text" placeholder='try "building mobile app"' /> 
-          </div>
-          <button>Search</button>
+            <div className="searchInput">
+              <img src="./img/search.png" alt="" />
+              <input type="text" placeholder='try "building mobile app"' />
+            </div>
+            <button>Search</button>
           </div>
           <div className="popular">
             <span>Popular:</span>
@@ -22,9 +22,9 @@ const Featured = () => {
             <button>AI Services</button>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <img src="./img/man.png" alt="" />
         </div>
-        .searchB
       </div>
     </div>
   )
---
+++ src/components/trustedBY/TrustedBy.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./TrustedBy.scss"
+
+const TrustedBy = () => {
+  return (
+    <div className='trustedBy'>
+        <div className="container">
+            
+        </div>
+    </div>
+  )
+}
+
+export default TrustedBy
